fc02664d3cc52ed1e04a4d576a774eb5ff410527
Nur-Alom <alamn7150@gmail.com>
2021-09-04 01:54:29 +0600

    first commit

---
+++ timeout.js
@@ -0,0 +1,11 @@
+
+function doSomething() {
+    console.log('i am a web devoloper');
+}
+
+console.log('First: ami sobar age');
+console.log('Second: ami 2o te');
+console.log('third: ami sobar ses a');
+setTimeout(doSomething);
+console.log('fourth: ami 3o te');
+console.log('Swcond: ami 4th o te');
